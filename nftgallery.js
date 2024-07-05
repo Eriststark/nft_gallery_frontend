e9a8b96a521f35935dd746fc20ec077b6ca7f189
Eriststark <99290946+Eriststark@users.noreply.github.com>
2024-07-05 16:16:56 -0400

    Update nftgallery.js

--- nftgallery.js
+++ nftgallery.js
@@ -7,18 +7,18 @@ document.addEventListener('DOMContentLoaded', async () => {
     const appId = '1k21orZXUK92MTql4kPB1cUfHW_y_wtBSVn7y260PCCG4vRQ8NZEkYfGCqkUk3TV';
     const contractAddress = "0xD7B368D30A17741C342BA8b7D41Ad4071BE9353A";
     const network = 80002;
-    //txhash of each nft minted 
-    const txhashes = [
-        "0xede903fe74e369a3059a6a4a66be7d6ce74ccef6be75e94fb6732b54a255b383",
-        "0xc60d9e8056e07bb4eb72328f45fb52925e94763187fbccfdf50b9af37f5f93e1",
-        "0xe5d5886ab486556a168ae6f7aa3eedec76626264d43f308595abb780a6f4c38b",
-        "0xb3a686379f4bd446d46856055a22dbe483822b9144c1a4d2c769bb7c62c63900",
-        "0x06c3194c862956160683fe925e99b0f4a40c86a87d2da23924eaa31f1d99e81b",
-        "0x7fef33dd8cd018337354e250f45db23f5a5a1b54bec6a6bfad40b23a4458495b",
-        "0xaf3441144245b8058196676034adbb6182a4b9cb8cd33062898ee95e54270170",
-        "0x280adbfbef264e5cb379645ec7ddbbc50066a476e9e2d92181164e73790325ae",
-        "0x1a6d31b0ed8797398209e814ff4ee22846c02c860aab7e4d6a4a03fac77dfb81",
-        "0xc1a4b7e8007090e8ea62fd22f0c5640119c420ec4f993b667cf4a30b5371cd00"
+    //details of each nft 
+    const nft_list = [
+    {id: 1, txhash: "0xede903fe74e369a3059a6a4a66be7d6ce74ccef6be75e94fb6732b54a255b383"},
+    {id: 2, txhash: "0xc60d9e8056e07bb4eb72328f45fb52925e94763187fbccfdf50b9af37f5f93e1"},
+    {id: 3, txhash: "0xe5d5886ab486556a168ae6f7aa3eedec76626264d43f308595abb780a6f4c38b"},
+    {id: 4, txhash: "0xb3a686379f4bd446d46856055a22dbe483822b9144c1a4d2c769bb7c62c63900"},
+    {id: 5, txhash: "0x06c3194c862956160683fe925e99b0f4a40c86a87d2da23924eaa31f1d99e81b"},
+    {id: 6, txhash: "0x7fef33dd8cd018337354e250f45db23f5a5a1b54bec6a6bfad40b23a4458495b"},
+    {id: 7, txhash: "0xaf3441144245b8058196676034adbb6182a4b9cb8cd33062898ee95e54270170"},
+    {id: 8, txhash: "0x280adbfbef264e5cb379645ec7ddbbc50066a476e9e2d92181164e73790325ae"},
+    {id: 9, txhash: "0x1a6d31b0ed8797398209e814ff4ee22846c02c860aab7e4d6a4a03fac77dfb81"},
+    {id: 10, txhash: "0xc1a4b7e8007090e8ea62fd22f0c5640119c420ec4f993b667cf4a30b5371cd00"}
     ];
 
     // Function to fetch metadata from API
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 }
 
-async function createNFTCard(uriData, metadata) {
+async function createNFTCard(uriData, metadata, txhash) {
     try {
         if (!uriData.image) {
             throw new Error('No image link found in URI data');
@@ -111,7 +111,7 @@ async function createNFTCard(uriData, metadata) {
         // Transaction Info Link
         let txinfoLinkContainer = document.createElement("div");
         let txinfoLink = document.createElement("a");
-        txinfoLink.href = `https://amoy.polygonscan.com/tx/${txhashes[item++]}`;
+        txinfoLink.href = `https://amoy.polygonscan.com/tx/${txhash}`;
         txinfoLink.target = "_blank";
         txinfoLink.textContent = "Transaction Info Link";
         txinfoLinkContainer.appendChild(txinfoLink);
@@ -127,8 +127,9 @@ async function createNFTCard(uriData, metadata) {
     // Main execution logic
     try {
         const bodies = [];
-        //id range depends on the ids you your nfts, the range yould should be continues
-        for (let id = 1; id <= 10; id++) {
+        for (let nft of nft_list) {
+            let id = nft["id"];
+            
             bodies.push({
                 contractAddress: contractAddress,
                 network: network,
@@ -138,12 +139,15 @@ async function createNFTCard(uriData, metadata) {
 
         // Sequentially process each NFT
         for (const body of bodies) {
+            const nft = nft_list[item++];
+            const txhash = nft["txhash"];
             const metadata = await fetchMetadata(body);
             const uriData = await fetchURI(metadata.uri);
-            await createNFTCard(uriData, metadata);
+            await createNFTCard(uriData, metadata, txhash);
         }
 
     } catch (error) {
         console.error('Error fetching NFTs:', error);
     }
 });
+
